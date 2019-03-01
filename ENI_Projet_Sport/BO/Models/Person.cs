9f5a47242623dc5b247cb7fad80be31e71644afe
Alex <a.delaunay97@gmail.com>
2019-03-01 10:25:09 +0100

    Bo, Services, Test

---
+++ ENI_Projet_Sport/BO/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using BO.Base;
+using BO.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BO.Models
+{
+    public class Person : BaseOV
+    {
+        [MaxLength(50)]
+        public virtual string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public virtual string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public virtual string PhoneNumber { get; set; }
+
+        public virtual DateTime BirthDate { get; set; }
+
+        public virtual DisplayConfiguration DisplayConfiguration { get; set; }
+
+        public virtual List<Race> Races { get; set; }
+
+        public Person()
+        {
+            Initialize();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            Races = new List<Race>();
+        }
+    }
+}
---
+++ ENI_Projet_Sport/ENI_Projet_Sport.Tests/ServiceRaceTypeTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENI_Projet_Sport.Tests
+{
+    public class ServiceRaceTypeTest
+    {
+        [TestMethod]
+        public void ServiceRaceTypeTest_Add()
+        {
+
+        }
+
+        [TestMethod]
+        public void ServiceRaceTypeTest_Update()
+        {
+
+        }
+
+        [TestMethod]
+        public void ServiceRaceTypeTest_Delete()
+        {
+
+        }
+
+        [TestMethod]
+        public void ServiceRaceTypeTest_GetById()
+        {
+
+        }
+
+        [TestMethod]
+        public void ServiceRaceTypeTest_GetAll()
+        {
+
+        }
+    }
+}
