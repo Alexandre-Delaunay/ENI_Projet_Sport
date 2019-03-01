2cac6f9ad030b783d770518188bb909710b19b1e
alexisgauvrit <alexis.gauvrit.85@gmail.com>
2019-03-01 09:53:38 +0100

    Update MapperConfig : Intitialize autoMapper

--- ENI_Projet_Sport/ENI_Projet_Sport/App_Start/MapperConfig.cs
+++ ENI_Projet_Sport/ENI_Projet_Sport/App_Start/MapperConfig.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BO.Models;
+using ENI_Projet_Sport.ViewModels;
 
 namespace ENI_Projet_Sport.App_Start
 {
     public class MapperConfig
-    {        
+    {
         public static void InitMapper()
         {
-                        
+            AutoMapper.Mapper.Initialize(config =>
+            {
+                // Model TO ViewModel
+                config.CreateMap<Race, RaceViewModel>();
+                config.CreateMap<Personne, PersonneViewModel>();
+                config.CreateMap<RaceType, RaceTypeViewModel>();
+                config.CreateMap<DisplayConfiguration, DisplayConfigurationViewModel>();
+                config.CreateMap<POI, POIViewModel>();
+                config.CreateMap<CategoryPOI, CategoryPOIViewModel>();
+                
+                // ViewModel TO Model
+                config.CreateMap<RaceViewModel, Race>();
+                config.CreateMap<PersonneViewModel, Personne>();
+                config.CreateMap<RaceTypeViewModel, RaceType>();
+                config.CreateMap<DisplayConfigurationViewModel, DisplayConfiguration>();
+                config.CreateMap<POIViewModel, POI>();
+                config.CreateMap<CategoryPOIViewModel, CategoryPOI>();
+
+                // Model TO CreateEditViewModel
+                config.CreateMap<Race, CreateEditRaceViewModel>();
+                config.CreateMap<POI, CreateEditPOIViewModel>();
+                config.CreateMap<Personne, CreateEditPersonneViewModel>();
+
+                // CreateEditViewModel TO Model
+                config.CreateMap<CreateEditRaceViewModel, Race>();
+                config.CreateMap<CreateEditPOIViewModel, POI>();
+                config.CreateMap<CreateEditPersonneViewModel, Personne>();
+            });
         }
     }
 }
---
+++ ENI_Projet_Sport/ENI_Projet_Sport/ViewModels/CreateEditPersonneViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ENI_Projet_Sport.ViewModels
+{
+    public class CreateEditPersonneViewModel
+    {
+    }
+}
