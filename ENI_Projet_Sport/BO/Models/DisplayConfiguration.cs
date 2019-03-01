723903a07fcc40875fe223754c5f02d342864feb
Alex <a.delaunay97@gmail.com>
2019-03-01 10:24:29 +0100

    BO, Services, Tests

--- ENI_Projet_Sport/BO/Models/DisplayConfiguration.cs
+++ ENI_Projet_Sport/BO/Models/DisplayConfiguration.cs
@@ -6,13 +6,8 @@ using System.Web;
 
 namespace BO.Models
 {
-    public class DisplayConfiguration: BaseOV
+    public class DisplayConfiguration : BaseOV
     {
-        public ApplicationUser ApplicationUser { get; set; }
-        public string DeviceName { get; set; }
-        public Guid Guid { get; set; }
-        public bool SpeedAvg { get; set; }
-        public bool SpeedMax { get; set; }
-        public UnitDistance UnitDistance { get; set; }
+        public virtual UnitDistance UnitDistance { get; set; }
     }
 }
---
+++ ENI_Projet_Sport/BO/Services/IServicePerson.cs
@@ -0,0 +1,19 @@
+﻿using BO.Base;
+using BO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO.Services
+{
+    public interface IServicePerson : IServiceBase
+    {
+        bool Add(Person personne);
+        bool Delete(Person personne);
+        bool Update(Person personne);
+        List<Person> GetAll();
+        Person GetById(int id);
+    }
+}
