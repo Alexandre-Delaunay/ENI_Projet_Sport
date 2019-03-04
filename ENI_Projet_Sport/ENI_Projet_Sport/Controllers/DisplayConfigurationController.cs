6b382c826da6fe4e5f7f0da28d63f8cae8cc9bdf
alexisgauvrit <alexis.gauvrit.85@gmail.com>
2019-03-04 12:24:00 +0100

    Add DisplayConfiguration Controller

---
+++ ENI_Projet_Sport/ENI_Projet_Sport/Controllers/DisplayConfigurationController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BO.Base;
+using BO.Models;
+using BO.Services;
+using ENI_Projet_Sport.Extensions;
+using ENI_Projet_Sport.ViewModels;
+
+namespace ENI_Projet_Sport.Controllers
+{
+    public class DisplayConfigurationController : Controller
+    {
+        private ServiceDisplayConfiguration serviceDisplayConfiguration = new ServiceDisplayConfiguration();
+
+        // GET: DisplayConfiguration/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DisplayConfiguration displayConfiguration = serviceDisplayConfiguration.GetById(Convert.ToInt32(id));
+            if (displayConfiguration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(displayConfiguration.Map<DisplayConfigurationViewModel>());
+        }
+
+        // POST: DisplayConfiguration/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(DisplayConfigurationViewModel displayConfigurationVM)
+        {
+            if (ModelState.IsValid)
+            {
+                displayConfigurationVM.DateMAJ = DateTime.Now;
+                serviceDisplayConfiguration.Update(displayConfigurationVM.Map<DisplayConfiguration>());
+                serviceDisplayConfiguration.Commit();
+                return RedirectToAction("Index");
+            }
+            return View(displayConfigurationVM);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            /*if (disposing)
+            {
+                db.Dispose();
+            }*/
+            base.Dispose(disposing);
+        }
+    }
+}
