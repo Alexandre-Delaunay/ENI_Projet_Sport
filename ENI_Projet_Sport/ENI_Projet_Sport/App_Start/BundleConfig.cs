1ed26cda825c9be8a0ed3ba639233f01c0bda164
alexisgauvrit <alexis.gauvrit.85@gmail.com>
2019-03-06 12:32:22 +0100

    Add jquery-confirm

--- ENI_Projet_Sport/ENI_Projet_Sport/App_Start/BundleConfig.cs
+++ ENI_Projet_Sport/ENI_Projet_Sport/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace ENI_Projet_Sport
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-confirm.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +26,8 @@ namespace ENI_Projet_Sport
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery-confirm.css"));
         }
     }
 }
