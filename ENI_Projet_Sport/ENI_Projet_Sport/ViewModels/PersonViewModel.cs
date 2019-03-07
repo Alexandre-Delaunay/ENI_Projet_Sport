867cc0085b3d555c6db53ce655cb27618db205dd
alexisgauvrit <alexis.gauvrit.85@gmail.com>
2019-03-07 16:20:42 +0100

    Update Validator

--- ENI_Projet_Sport/ENI_Projet_Sport/ViewModels/PersonViewModel.cs
+++ ENI_Projet_Sport/ENI_Projet_Sport/ViewModels/PersonViewModel.cs
@@ -16,8 +16,11 @@ namespace ENI_Projet_Sport.ViewModels
         [Required, DisplayName("Nom")]
         public virtual string LastName { get; set; }
         [DisplayName("Numéro de téléphone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0]{1})([0-9]{1})\)?[-. ]?([0-9]{2})[-. ]?([0-9]{2})[-. ]?([0-9]{2})[-. ]?([0-9]{2})$", ErrorMessage = "Ce n'est pas un numéro de téléphone valide")]
         public virtual string PhoneNumber { get; set; }
         [Required, DisplayName("Date de naissance")]
+        [DataType(DataType.Date)]
         public virtual DateTime BirthDate { get; set; }
         public virtual List<Race> Races { get; set; }
         public PersonViewModel()
